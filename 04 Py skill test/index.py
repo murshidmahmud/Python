2db406e986af427de95a1882d84c3e575958cdf9
Murshid <murshidmahmud4@gmail.com>
2026-05-24 20:11:57 +0600

    24-05-2026

---
+++ 04 Py skill test/index.py
@@ -0,0 +1,36 @@
+print(8)
+print(13, end=" ")
+print(21)
+print("\n")                         #Line break
+
+print("1", "-", 1)
+print("2", "-", 2*2)
+print("3", "-", 3*3)
+print("4", "-", 4*4)
+print("5", "-", 5*5)
+
+print("\n")                         #Line break
+for i in range(4):
+    print("****")
+
+print("\n")                        #Line break
+print(7.0/2)
+
+print("\n")                        #Line break
+x = 20
+y = 6
+print(x//y)
+
+
+print("\n")                       #Line break
+temperature = 25.5
+
+print("Celsius -",temperature)
+print("Kelvin -",temperature + 273)
+
+print("\n")                              #Line break
+
+side = 4.5
+
+print(side*side)
+print(4*side)
