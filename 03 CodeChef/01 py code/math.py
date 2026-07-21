603b6cd59cbdbb95376166becfb31968c18e6a02
Murshid Mahmud <murshidmahmud4@gmail.com>
2026-07-22 00:38:33 +0600

    py

---
+++ 03 CodeChef/01 py code/math.py
@@ -0,0 +1,17 @@
+num1 = 40
+num2 = 10
+
+Addition = num1+num2
+print(Addition)             #Ans: 50
+
+subtraction = num1 - num2
+print(subtraction)            #Ans: 30
+
+Multiplication = num1 * num2
+print(Multiplication)           #Ans: 400
+
+Division = num1 / num2
+print(Division)                #Ans: 4.0
+ 
+Modulo = num1 % num2 
+print(Modulo)                #Ans: 0
